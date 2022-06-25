9f51f58e7059621efc212a43481b855f54a7edf7
Mouse <mouse0w0@qq.com>
2022-06-25 23:11:00 +0800

    optimize PathParser

--- configuration-api/src/main/java/configuration/PathParser.java
+++ configuration-api/src/main/java/configuration/PathParser.java
@@ -1,39 +1,38 @@
 package configuration;
 
-import configuration.util.StringUtils;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class PathParser {
+    private static final String[] EMPTY_STRING_ARRAY = new String[0];
 
     public static String[] parse(String path, ConfigOptions options) {
         return parse(path, options.getPathSeparator(), options.getKeyValidator());
     }
 
     public static String[] parse(String path, char pathSeparator, Pattern keyValidator) {
-        if (StringUtils.isNullOrEmpty(path)) {
-            return new String[0];
+        if (path == null || path.isEmpty()) {
+            return EMPTY_STRING_ARRAY;
         }
 
-        List<String> keys = new ArrayList<>();
-        StringBuilder name = new StringBuilder();
+        final List<String> keys = new ArrayList<>();
+        final StringBuilder key = new StringBuilder();
 
         for (char c : path.toCharArray()) {
             if (c == pathSeparator) {
-                if (!keyValidator.matcher(name).matches()) {
+                if (!keyValidator.matcher(key).matches()) {
                     throw new InvalidPathException(path, c);
                 }
 
-                keys.add(name.toString());
-                name = new StringBuilder();
+                keys.add(key.toString());
+                key.setLength(0);
             } else {
-                name.append(c);
+                key.append(c);
             }
         }
-        keys.add(name.toString());
+        keys.add(key.toString());
 
-        return keys.toArray(new String[0]);
+        return keys.toArray(EMPTY_STRING_ARRAY);
     }
 }
--- configuration-api/src/main/java/configuration/util/StringUtils.java
+++
@@ -1,7 +0,0 @@
-package configuration.util;
-
-public class StringUtils {
-    public static boolean isNullOrEmpty(String value) {
-        return value == null || value.isEmpty();
-    }
-}
