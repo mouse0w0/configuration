04c5eee640a875e09cf6bcb553e731c82ef236da
Mouse <mouse0w0@qq.com>
2022-06-25 23:09:40 +0800

    suppress warning for Config

--- configuration-api/src/main/java/configuration/Config.java
+++ configuration-api/src/main/java/configuration/Config.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
 
+@SuppressWarnings("unchecked")
 public final class Config {
 
     private final ConfigOptions options;
